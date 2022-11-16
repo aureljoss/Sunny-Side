384533a9c4d34c14c503f49c3a42ec82211740f5
aureljoss <josserand.n.aurelie@gmail.com>
2022-11-15 22:50:26 -0500

    Updates mobile nav. Hides menu when link clicked

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,26 @@
 const menu=document.getElementById("menu-hide");
 const hamburger=document.getElementById("hamburger");
+const one=document.getElementById("one");
+const two=document.getElementById("two");
+const three=document.getElementById('three');
+const four=document.getElementById('four');
 
 hamburger.addEventListener("click", () => {
     menu.style.display = "flex";
+  });
+
+  one.addEventListener("click", () => {
+    menu.style.display = "none";
+  });
+
+  two.addEventListener("click", () => {
+    menu.style.display = "none";
+  });
+
+  three.addEventListener("click", () => {
+    menu.style.display = "none";
+  });
+
+  four.addEventListener("click", () => {
+    menu.style.display = "none";
   });
